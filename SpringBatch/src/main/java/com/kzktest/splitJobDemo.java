f7716fe9063b36ca160d8581aa5737a84d803827
kanzikun <kanzikun@kzk.com>
2021-01-02 23:41:49 +0800

    代码提交

---
+++ SpringBatch/src/main/java/com/kzktest/splitJobDemo.java
@@ -0,0 +1,90 @@
+package com.kzktest;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+@EnableBatchProcessing
+public class splitJobDemo {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step splitJobStep1(){
+        return stepBuilderFactory.get("splitJobStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("splitJobStep1");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Step splitJobStep2(){
+        return stepBuilderFactory.get("splitJobStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("splitJobStep2");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Step splitJobStep3(){
+        return stepBuilderFactory.get("SplitJobStep3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("SplitJobStep3");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    //创建flow
+    @Bean
+    public Flow splitDemoFlow1(){
+        return new FlowBuilder<Flow>("splitDemoFlow1")
+                .start(splitJobStep1())
+                .build();
+    }
+
+    @Bean
+    public Flow splitDemoFlow2(){
+        return new FlowBuilder<Flow>("splitDemoFlow2")
+                .start(splitJobStep2())
+                .next(splitJobStep3())
+                .build();
+    }
+
+    @Bean
+    public Job splitDemoJob(){
+        return jobBuilderFactory.get("splitDemoJob")
+                .start(splitDemoFlow1())
+                .split(new SimpleAsyncTaskExecutor())
+                .add(splitDemoFlow2())
+                .end()
+                .build();
+    }
+}
